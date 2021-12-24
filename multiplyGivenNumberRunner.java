f1d9bd5c76fe69c15cc2b516f8f2a5fd72ba05b4
gayathristuthipai <gayathristuthipai@gmail.com>
2021-12-24 16:34:37 +0530

    Multiplication table for given number committed.

---
+++ multiplyGivenNumberRunner.java
@@ -0,0 +1,21 @@
+package fullStackJavaCourse;
+
+public class multiplyGivenNumberRunner
+{
+	public static void multiply(int num) 
+	{
+		for(int i=1;i<=10;i++)
+		{
+		System.out.printf("%d * %d = %d", num,i,num*i).println();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		multiply(5);
+		
+
+	}
+
+}
